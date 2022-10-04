c1a7c05c29dac651d1a82d4dd038684f3486d1f1
omorodion <omorodion.fortune@gmail.com>
2022-10-04 09:50:43 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,33 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include "main.h"
 
 /**
- * main - prints the minimum number of coins to make change for an amount of money
- * @argc: number of arguments passed to the function
- * @argv: argument vector of pointers to strings
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: 0 if no errors, else 1
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-	int a = 0, i, j;
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	for (i = 1; i < argc; i++)
+	if (argc != 2)
 	{
-		for (j = 0; argv[i][j]; j++)
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
 		{
-			if (isdigit(argv[i][j]) == 0)
-			{
-				puts("Error");
-				return (1);
-			}
+			result++;
+			num -= coins[j];
 		}
 	}
-	for (i = 1; i < argc; i++)
-	{
-		a += atoi(argv[i]);
-	}
-	printf("%d\n", a);
+
+	printf("%d\n", result);
 	return (0);
 }
